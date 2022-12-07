6d34a43011cf3e3aa3a1409a224cdb3167e14079
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-07 16:15:40 +0200

    Step_4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,4 +1,6 @@
-let x = window.prompt("Enter your Name: ")
-let y = window.prompt("Enter your SurName: ")
-let z = window.prompt("Enter your City: ")
-alert("Hello,\n"+"Name : "+x+"\n"+"SurName : "+y+"\n"+"City :"+z)
+function ahmad(){ 
+    let x = document.getElementById("name").value;
+    let y = document.getElementById("surname").value;
+    let z = document.getElementById("city").value;
+alert("Hello,\n"+"Name : "+x+"\n"+"SurName : "+y+"\n"+"City :"+z);
+}
