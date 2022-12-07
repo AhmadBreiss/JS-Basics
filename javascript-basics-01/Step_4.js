61bf92ac93520977c652eae16a9065ff32a00c29
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-07 11:31:51 +0200

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let x = window.prompt("Enter your Name: ")
+let y = window.prompt("Enter your SurName: ")
+let z = window.prompt("Enter your City: ")
+alert("Hello ,\n"+"Name : "+x+"\n"+"SurName : "+y+"\n"+"City :")
