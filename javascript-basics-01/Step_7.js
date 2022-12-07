2f5a6640da2a77de73ab1504b9230340fcb90be1
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-07 15:27:57 +0200

    step_7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,4 @@
+let x = window.prompt("Enter your shoe size: ")
+let y = window.prompt("Enter your birth year: ")
+let z = (x*2+(5))*(50)-y+1766
+alert ("(" + x + "*2+5)*(50)-" + y + "+ 1766 = " + z)
