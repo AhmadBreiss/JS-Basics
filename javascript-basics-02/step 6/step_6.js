3fd87423d832ac280f7cef4419912b5f9cad2f8c
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-08 16:51:21 +0200

    step6

---
+++ javascript-basics-02/step 6/step_6.js
@@ -0,0 +1,7 @@
+let x=document.querySelectorAll("img")
+ for (let i=0; i<=x.length ; i++){
+    x[i].addEventListener("mouseover",function(){document.getElementById("image"+(i+1)).src="images/image"+(i+1)+"_2.jpg"
+ })
+    x[i].addEventListener("mouseleave",function(){document.getElementById("image"+(i+1)).src="images/image"+(i+1)+".jpg"
+ })
+}
